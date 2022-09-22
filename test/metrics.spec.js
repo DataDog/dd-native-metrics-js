8e613ecc4efcb4d4f6bd5ecb237eab7564b6b53a
rochdev <roch.devost@gmail.com>
2022-09-22 15:19:43 -0400

    fix imprecision with event loop stats test

--- test/metrics.spec.js
+++ test/metrics.spec.js
@@ -111,8 +111,8 @@ describe('metrics', () => {
       const stats = nativeMetrics.stats()
 
       expect(stats.eventLoop.count).to.be.gte(1)
-      expect(stats.eventLoop.max).to.be.gte(90 * 1e6)
-      expect(stats.eventLoop.sum).to.be.gte(90 * 1e6)
+      expect(stats.eventLoop.max).to.be.gte(50 * 1e6)
+      expect(stats.eventLoop.sum).to.be.gte(50 * 1e6)
 
       done()
     })
