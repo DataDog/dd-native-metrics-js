6b623607e79e1e61ddb011d639bfcde1b24443ac
Attila Szegedi <attila.szegedi@datadoghq.com>
2025-03-17 13:23:02 +0100

    Extract a method needing FP operations

--- src/metrics/Heap.hpp
+++ src/metrics/Heap.hpp
@@ -16,6 +16,11 @@ namespace datadog {
       Value ToJSON(Env env);
   };
 
+  // Extracted as it needs FP operations
+  void setLong(Object& obj, const char* key, uint64_t value) {
+    obj.Set(key, value);
+  }
+
   GENERAL_REGS_ONLY Value Heap::ToJSON(Env env) {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
@@ -25,10 +30,10 @@ namespace datadog {
       if (isolate->GetHeapSpaceStatistics(&stats, i)) {
         Object space = Object::New(env);
         space.Set("space_name", stats.space_name());
-        space.Set("space_size", stats.space_size());
-        space.Set("space_used_size", stats.space_used_size());
-        space.Set("space_available_size", stats.space_available_size());
-        space.Set("physical_space_size", stats.physical_space_size());
+        setLong(space, "space_size", stats.space_size());
+        setLong(space, "space_used_size", stats.space_used_size());
+        setLong(space, "space_available_size", stats.space_available_size());
+        setLong(space, "physical_space_size", stats.physical_space_size());
         spaces.Set(i, space);
       }
     }
