a9e598c1c7957075fb878c3e9aebab22fc99a144
Roch Devost <roch.devost@gmail.com>
2025-04-07 19:15:55 -0400

    Revert "Properly finalize elimination of FP use for pointer ops (#70)"
    This reverts commit 104d4e4b5d852d893a7defb178f4828b826f2999.

--- src/metrics/Heap.hpp
+++ src/metrics/Heap.hpp
@@ -16,12 +16,7 @@ namespace datadog {
       Value ToJSON(Env env);
   };
 
-  // Extracted as it needs FP operations
-  void setLong(Object& obj, const char* key, uint64_t value) {
-    obj.Set(key, value);
-  }
-
-  GENERAL_REGS_ONLY Value Heap::ToJSON(Env env) {
+  Value Heap::ToJSON(Env env) GENERAL_REGS_ONLY {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
     Array spaces = Array::New(env);
@@ -30,10 +25,10 @@ namespace datadog {
       if (isolate->GetHeapSpaceStatistics(&stats, i)) {
         Object space = Object::New(env);
         space.Set("space_name", stats.space_name());
-        setLong(space, "space_size", stats.space_size());
-        setLong(space, "space_used_size", stats.space_used_size());
-        setLong(space, "space_available_size", stats.space_available_size());
-        setLong(space, "physical_space_size", stats.physical_space_size());
+        space.Set("space_size", stats.space_size());
+        space.Set("space_used_size", stats.space_used_size());
+        space.Set("space_available_size", stats.space_available_size());
+        space.Set("physical_space_size", stats.physical_space_size());
         spaces.Set(i, space);
       }
     }
