73a5701f6c9c00b8d33e556261b70d3ba5f4c267
Roch Devost <roch.devost@datadoghq.com>
2025-04-08 11:19:29 -0400

    revert fp workarounds (#72)
    * Revert "Properly finalize elimination of FP use for pointer ops (#70)"
    
    This reverts commit 104d4e4b5d852d893a7defb178f4828b826f2999.
    
    * Revert "Eliminate use of FP registers for pointers (#68)"
    
    This reverts commit b5f4ac23eb48bd6a089405cd973814c4793368ec.

--- src/metrics/main.cpp
+++ src/metrics/main.cpp
@@ -4,7 +4,6 @@
 #include "GarbageCollection.hpp"
 #include "Heap.hpp"
 #include "Process.hpp"
-#include "general-regs-only.hpp"
 
 using Napi::Addon;
 using Napi::CallbackInfo;
@@ -21,7 +20,7 @@ namespace datadog {
       // This needs to be a pointer so it can live longer than env for uv_close
       EventLoop* loopInfo;
 
-      GENERAL_REGS_ONLY Value Start(const CallbackInfo& info) {
+      Value Start(const CallbackInfo& info) {
         if (info.Length() > 0) { // Enable only selected watchers
           for (size_t i = 0; i < info.Length(); i++) {
             std::string watcher = info[i].As<Napi::String>().Utf8Value();
